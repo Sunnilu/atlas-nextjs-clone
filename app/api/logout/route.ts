270abdb33d9bf9b9e261759f1ed13d149e1bb62b
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-04 21:46:41 -0500

    logout

---
+++ app/api/logout/route.ts
@@ -0,0 +1,8 @@
+// app/api/logout/route.ts
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+  const res = NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'));
+  res.cookies.set('session', '', { maxAge: 0 });
+  return res;
+}
