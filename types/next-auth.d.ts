fd45217723abab65d5bacaf87396faa220a2acc5
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-11 21:51:33 -0500

    kept throwing error for session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,20 +1,21 @@
 // types/next-auth.d.ts
 import NextAuth from "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
-  interface Session {
+  interface Session extends DefaultSession {
     user: {
       id: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+    } & DefaultSession["user"];
   }
 
-  interface User {
+  interface User extends DefaultUser {
     id: string;
-    name?: string | null;
-    email?: string | null;
-    image?: string | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
   }
 }
