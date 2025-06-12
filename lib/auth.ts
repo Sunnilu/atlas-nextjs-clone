1cbb7394d4fa73de834cde0d4a970b38c32220dc
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-11 21:46:05 -0500

    fully working session and next auth

---
+++ lib/auth.ts
@@ -0,0 +1,22 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+// ✅ Define config
+export const authConfig: NextAuthConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      // @ts-expect-error - token.sub is guaranteed to exist
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+};
+
+// ✅ Export the NextAuth handler
+export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
